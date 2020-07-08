6c7a2513fdfa484cf00e5e4bb4adb0668ca7dfd4
RekGRpth <rekgrpth@gmail.com>
2020-07-08 16:40:47 +0500

    up

--- my.sh
+++ my.sh
@@ -6,10 +6,10 @@ wget -c https://rossvyaz.ru/data/ABC-3xx.csv && \
 wget -c https://rossvyaz.ru/data/ABC-4xx.csv && \
 wget -c https://rossvyaz.ru/data/ABC-8xx.csv && \
 wget -c https://rossvyaz.ru/data/DEF-9xx.csv && \
-iconv -f cp1251 -t utf-8 ABC-3xx.csv -o ABC-3xx.csv.tmp && mv -f ABC-3xx.csv.tmp ABC-3xx.csv && \
-iconv -f cp1251 -t utf-8 ABC-4xx.csv -o ABC-4xx.csv.tmp && mv -f ABC-4xx.csv.tmp ABC-4xx.csv && \
-iconv -f cp1251 -t utf-8 ABC-8xx.csv -o ABC-8xx.csv.tmp && mv -f ABC-8xx.csv.tmp ABC-8xx.csv && \
-iconv -f cp1251 -t utf-8 DEF-9xx.csv -o DEF-9xx.csv.tmp && mv -f DEF-9xx.csv.tmp DEF-9xx.csv && \
+#iconv -f cp1251 -t utf-8 ABC-3xx.csv -o ABC-3xx.csv.tmp && mv -f ABC-3xx.csv.tmp ABC-3xx.csv && \
+#iconv -f cp1251 -t utf-8 ABC-4xx.csv -o ABC-4xx.csv.tmp && mv -f ABC-4xx.csv.tmp ABC-4xx.csv && \
+#iconv -f cp1251 -t utf-8 ABC-8xx.csv -o ABC-8xx.csv.tmp && mv -f ABC-8xx.csv.tmp ABC-8xx.csv && \
+#iconv -f cp1251 -t utf-8 DEF-9xx.csv -o DEF-9xx.csv.tmp && mv -f DEF-9xx.csv.tmp DEF-9xx.csv && \
 python3 my1.py && \
 python3 my2.py && \
 ant -f java/build.xml build-geo-data
