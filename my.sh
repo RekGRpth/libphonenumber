88f2945892b344deb4b0c10e21480a2025536589
RekGRpth <rekgrpth@gmail.com>
2021-02-09 16:25:37 +0500

    up

--- my.sh
+++ my.sh
@@ -2,10 +2,11 @@
 
 find resources/geocoding -mindepth 1 -maxdepth 1 -type d ! -name "ru" ! -name "en" -exec rm -rf '{}' \; && \
 rm -f ABC-3xx.csv ABC-4xx.csv ABC-8xx.csv DEF-9xx.csv && \
-wget -c https://rossvyaz.ru/data/ABC-3xx.csv && \
-wget -c https://rossvyaz.ru/data/ABC-4xx.csv && \
-wget -c https://rossvyaz.ru/data/ABC-8xx.csv && \
-wget -c https://rossvyaz.ru/data/DEF-9xx.csv && \
+#https://rossvyaz.gov.ru/deyatelnost/resurs-numeracii/vypiska-iz-reestra-sistemy-i-plana-numeracii \
+wget -c https://rossvyaz.gov.ru/data/ABC-3xx.csv --no-check-certificate --output-document=ABC-3xx.csv && \
+wget -c https://rossvyaz.gov.ru/data/ABC-4xx.csv --no-check-certificate --output-document=ABC-4xx.csv && \
+wget -c https://rossvyaz.gov.ru/data/ABC-8xx.csv --no-check-certificate --output-document=ABC-8xx.csv && \
+wget -c https://rossvyaz.gov.ru/data/DEF-9xx.csv --no-check-certificate --output-document=DEF-9xx.csv && \
 #iconv -f cp1251 -t utf-8 ABC-3xx.csv -o ABC-3xx.csv.tmp && mv -f ABC-3xx.csv.tmp ABC-3xx.csv && \
 #iconv -f cp1251 -t utf-8 ABC-4xx.csv -o ABC-4xx.csv.tmp && mv -f ABC-4xx.csv.tmp ABC-4xx.csv && \
 #iconv -f cp1251 -t utf-8 ABC-8xx.csv -o ABC-8xx.csv.tmp && mv -f ABC-8xx.csv.tmp ABC-8xx.csv && \
